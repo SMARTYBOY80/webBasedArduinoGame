9c6144232400db0bab6d02a3e09b13c6eea36f7a
Jack Smart <89162222+SMARTYBOY80@users.noreply.github.com>
2025-11-07 21:36:08 +0000

    added pipes which appear every 150 frames todo add collision and add pipe culling

--- frontend/app/games/FlappyBird.tsx
+++ frontend/app/games/FlappyBird.tsx
@@ -1,19 +1,16 @@
 "use client";
 
 import { useEffect, useState } from "react";
-;
 
 
-// Tetris Game Constants
-// data from https://tetris.wiki/Tetris_Guideline
-
+// Game Board dimensions
 const boardWidth = 900;
 const boardHeight = 600;
 
 let gameOver = false;
 
 
-// Tetris Component
+// Flappy Bird Component
 export default function FlappyBird() {
 
     // State variables for score
@@ -41,47 +38,150 @@ export default function FlappyBird() {
 
         const gravity = 0.5;
 
+        // pipe variables
+        const pipeGap = 75;
+        const minPipeHeight = 20;
+
+        let pipeSpeed = 2
+
+        // so it wont clip the floor
+        const maxPipeHeight = boardHeight - pipeGap - 240;
+
+        const pipes: Array<topPipe | bottomPipe> = [];
+
 
         // bird varibles
         class Bird {
-            constructor(y : float, velocity : float) {
+            x : number;
+            // todo reserch why are all numbers defined as number instead of int or float
+            y : number;
+            size : number;
+            velocity : number;
+
+            constructor(y : number, velocity : number) {
                 this.x = 40;
                 this.y = y;
-                this.width = 20;
                 this.size = 20;
                 this.velocity = velocity;
             }
 
-            birdFlap() {
-                this.velocity = -6;
+            flap() {
+                this.velocity = -10;
             }
 
         }
+
+        class topPipe {
+            x: number;
+            y: number;
+            width: number;
+            height: number;
+
+            constructor(x: number, y: number, height: number) {
+                this.x = x;
+                this.y = y;
+                this.width = 50;
+                this.height = height;
+            }
+        }
+
+        class bottomPipe {
+            x: number;
+            y: number;
+            width: number;
+            height: number;
+
+            constructor(x: number, y: number, height: number) {
+                this.x = x;
+                this.y = y;
+                this.width = 50;
+                this.height = height;
+            }
+        }
+
 
         const bird = new Bird(150, 0);
 
         // draw scene
 
-        function drawScene(ctx : CanvasRenderingContext2D) {
-            // clear canvas
-            ctx.clearRect(0, 0, boardWidth, boardHeight);
 
+        function drawGround(ctx : CanvasRenderingContext2D) {
+            // draw ground
+            ctx.fillStyle = "lightGreen";
+            ctx.fillRect(0, boardHeight - 100, boardWidth, 100);
+        }
+
+        function drawBird(ctx : CanvasRenderingContext2D){
             // draw bird
             ctx.fillStyle = "yellow";
             ctx.fillRect(bird.x, bird.y, bird.size, bird.size);
 
         }
 
+        function drawPipes(ctx : CanvasRenderingContext2D) {
+            // draw pipes
+            ctx.fillStyle = "green";
+            for (const pipe of pipes) {
+                ctx.fillRect(pipe.x, pipe.y, pipe.width, pipe.height);
+            }
+        }
 
+        function drawScene(ctx : CanvasRenderingContext2D) {
+            // clear canvas
+            ctx.clearRect(0, 0, boardWidth, boardHeight);
+
+            drawBird(ctx);
+
+            drawPipes(ctx);
+
+            drawGround(ctx);
+        }
+
+        function makePipe() {
+
+            const topPipeHeight = Math.floor(Math.random() * (maxPipeHeight - minPipeHeight + 1)) + minPipeHeight ;
+            const bottomPipeHeight = boardHeight - topPipeHeight - pipeGap - 100;
+
+            const topPipeY = 0;
+            const bottomPipeY = boardHeight - bottomPipeHeight;
+
+            const pipeX = boardWidth;
+            const topPipeObj = new topPipe(pipeX, topPipeY, topPipeHeight);
+            const bottomPipeObj = new bottomPipe(pipeX, bottomPipeY, bottomPipeHeight);
+
+            pipes.push(topPipeObj);
+            pipes.push(bottomPipeObj);
+        }
+
+        function movePipes() {
+            for (const pipe of pipes) {
+                pipe.x -= pipeSpeed;
+            }
+        }
 
         // game loop
+        let lastPipeTime = 0;
+        makePipe();
         function update(time = 0) {
             if (ctx == null ){return}
             drawScene(ctx)
 
-            bird.velocity += gravity
-            bird.y += bird.velocity
+            bird.velocity += gravity;
+            bird.y += bird.velocity;
 
+            console.log(typeof (lastPipeTime));
+
+            lastPipeTime += 1;
+
+            // move pipes
+            movePipes();
+
+            // add new pipe every 150 frames
+            if (lastPipeTime > 150) {
+                console.log(time);
+                makePipe();
+                lastPipeTime = 0;
+            }
 
 
 
@@ -102,7 +202,7 @@ export default function FlappyBird() {
 
         document.addEventListener('keydown', function(event) {
             if (event.key === " ") {
-                bird.velocity = -10;
+                bird.flap()
             }
         });
 
